37a5682275b4685a20a384a81813d322ca56f6ed
munnuspider <bhavya.trivedi2007@gmail.com>
2026-03-23 21:59:34 +0000

    yes

--- worksheet/task2/question4.sql
+++ worksheet/task2/question4.sql
@@ -5,5 +5,5 @@
 SELECT s.StudentId, s.FirstName, s.LastName, c.CourseName
 FROM Student s
 JOIN Enrolment e ON s.StudentId = e.StudentId
-JOIN JOIN Course c ON e.CourseId = c.CourseId;
+JOIN Course c ON e.CourseId = c.CourseId;
 
